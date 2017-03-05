0602de6d50e13d2f92ca60348fb6e3680f7f01fd
seanphunt18 <seanphunt18@gmail.com>
2017-03-05 17:00:51 -0500

    question feedback working

---
+++ app.js
@@ -0,0 +1,70 @@
+var state = {
+    answers: [{text: "A. Alex Ovechkin", correct: true, default: true}, {text: "B. Alex Ovechkin", correct: false, default: true}, {text: "C. Alex Ovechkin", correct: false, default: true}, {text: "D. Alex Ovechkin", correct: false, default: true}]
+};
+
+var correctAnswers = 0;
+
+var incorrectAnswers = 0;
+
+var questionCount = 1;
+
+// State modification functions
+var checkAnswer = function(state, answerText) {
+	for (var i=0; i<state.answers.length; i++) {
+		if (state.answers[i].text == answerText) {
+			state.answers[i].default = !state.answers[i].default;
+			if (state.answers[i].correct) {
+				correctAnswers ++;
+			} else {
+				incorrectAnswers ++;
+			}
+		} else if (state.answers[i].correct) {
+			state.answers[i].default = !state.answers[i].default;
+		}
+	}
+}
+
+
+
+// Render function
+
+var renderList = function(state, element) {
+    var itemsHTML = state.answers.map(function(answer) {
+
+        var beginListItem = "";
+
+        if (answer.default) {
+        	 beginListItem = '<li><button type="submit" class="answer default">';
+        } else if (answer.correct) {
+            beginListItem = '<li><button type="submit" class="answer correct">';
+        } else {
+            beginListItem = '<li><button type="submit" class="answer incorrect">';
+        }
+
+        return beginListItem + answer.text + '</button></li>';
+    });
+    element.html(itemsHTML);
+    $('.js-amt-correct').text(correctAnswers);
+    $('.js-amt-incorrect').text(incorrectAnswers);
+    $('.js-question-count').text(questionCount);
+};
+
+// Event listeners
+
+$(function() { 
+
+    // $('#js-enter-button').on('click', function(event) {
+    // 	$('.landing').closest('div').addClass("hidden");
+    // });
+
+    renderList(state, $('.js-list'));
+
+    $('.answer').on('click', function(event) {
+    	event.preventDefault;
+    	checkAnswer(state, $(this).closest("li").find('.answer').text());;
+
+    	renderList(state, $('.js-list'));
+    });
+
+
+});
